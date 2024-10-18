cda3087ca685d8bd896cba656c75e01ad0b17799
BlockOG <alesiobaca@gmail.com>
2024-10-18 16:07:07 +0300

    update rating calculations to 3.6.0

--- common.js
+++ common.js
@@ -3,27 +3,25 @@ const difficulty_names = ["opening", "middle", "finale", "encore"];
 
 function ratingFromScore(cc, score, bonus) {
     let res = -Infinity;
-    if (1008000 < score) res = 2000;
-    else if (1000000 < score) res = 1500 + (500 * (score - 1000000)) / 8000;
-    else if (980000 < score) res = 1000 + (500 * (score - 980000)) / 20000;
-    else if (950000 < score) res = (1000 * (score - 950000)) / 30000;
-    else if (600000 < score) res = -7000 + (7000 * (score - 600000)) / 350000;
+    if (1008000 <= score) res = 1800;
+    else if (1000000 <= score) res = 1000 + (800 * (score - 1000000)) / 8000;
+    else if (980000 <= score) res = (1000 * (score - 980000)) / 20000;
+    else if (600000 <= score) res = -8000 + (8000 * (score - 600000)) / 380000;
 
-    return Math.max(0, cc * 1000 + res - 500) + bonus * 250;
+    return Math.max(0, cc * 1000 + res) + bonus * 100;
 }
 
 function scoreFromRating(cc, rating, bonus) {
     if (rating < 0) return;
     else if (rating == 0) return 0;
 
-    rating -= Math.max(0, cc * 1000 - 500) + bonus * 250;
+    rating -= Math.max(0, cc * 1000) + bonus * 100;
 
     let res = 0;
-    if (2000 < rating) return;
-    else if (1500 < rating) res = 1000000 + (8000 * (rating - 1500)) / 500;
-    else if (1000 < rating) res = 980000 + (20000 * (rating - 1000)) / 500;
-    else if (0 < rating) res = 950000 + (30000 * rating) / 1000;
-    else if (-7000 < rating) res = 600000 + (350000 * (rating - -7000)) / 7000;
+    if (1800 < rating) return;
+    else if (1000 <= rating) res = 1000000 + (8000 * (rating - 1000)) / 800;
+    else if (0 <= rating) res = 980000 + (20000 * rating) / 1000;
+    else if (-8000 <= rating) res = 600000 + (380000 * (rating - -8000)) / 8000;
     else return;
 
     return res;
